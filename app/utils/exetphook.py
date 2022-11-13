999e7e05efd4611cdf2f7c8c46e6d16f0916d345
G1ebS <glev.shooter06@gmail.com>
2022-11-13 16:44:46 +0300

    Реализовано: 1) Поиск 2) Exepthook 3) Реализацтя таблиц 4) Страница профиля

---
+++ app/utils/exetphook.py
@@ -0,0 +1,13 @@
+import sys
+import traceback
+from PyQt6 import QtWidgets
+
+
+def excepthook(exc_type, exc_value, exc_tb):
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    print("error catched!:")
+    print("error message:\n", tb)
+    QtWidgets.QApplication.quit()
+
+
+sys.excepthook = excepthook
